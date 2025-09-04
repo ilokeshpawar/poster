059795d605b2a9e7b3e3c20ac16eed29632e80bd
Lokesh <ilokeshpawar@gmail.com>
2025-09-05 04:35:00 +0530

    minor fixes

--- src/poster/constants.py
+++ src/poster/constants.py
@@ -5,4 +5,4 @@ LINKEDIN_PROFILE_SIZE: Final = (400, 400)
 LINKEDIN_COVER_SIZE: Final = (1400, 350)  # NOTE - Keeping the aspect ratio of 4:1
 
 LOGO_SIZE: Final = (25, 25)
-LINKEDIN_COVER_FILE_NAME: Final = "output/example_linkedin.png"
+LINKEDIN_COVER_FILE_PATH: Final = "output/example_linkedin.png"
