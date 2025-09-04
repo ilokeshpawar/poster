2590b5047c414132c94149ec3b385d9a98a97c0e
Lokesh <ilokeshpawar@gmail.com>
2025-09-05 04:38:58 +0530

    refactoring; more modular approach; few add-ons; more config reliant

--- src/poster/main.py
+++ src/poster/main.py
@@ -2,19 +2,18 @@ import os
 import subprocess
 from pathlib import Path
 from tomllib import load as toml_load
-from typing import Tuple
+from typing import NewType, Tuple
 
-# from collections.abc import Sequence
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, UnidentifiedImageError
 from rich import print
 
 from poster.constants import LOGO_SIZE
 
+RgbHex = NewType("RgbHex", str)
 
-def know_the_size(image_path: Path) -> None:
-    """Return the dimensions (width, height) of the image at the given path."""
-    # TODO - Implement this function
-    pass
+
+def rgb_to_hex(rgb: tuple[int, int, int]) -> RgbHex:
+    return RgbHex(f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}")
 
 
 def load_config(config_path: Path) -> dict:
@@ -24,17 +23,31 @@ def load_config(config_path: Path) -> dict:
     return config
 
 
+def know_the_size() -> None:
+    """Return the dimensions (width, height) of the image at the given path."""
+    config = load_config(Path("config/config.toml"))
+    images = config["picture"]
+    for image in images.keys():
+        image_path = images[image][0]
+        try:
+            with Image.open(image_path) as img:
+                width, height = img.size
+                print(f"Dimensions of {image} ({image_path}): {width}x{height}")
+        except UnidentifiedImageError:
+            print(f"Skipping non-image file: {image_path}")
+
+
 def svg_to_png(
-    svg_path: Path, png_path: Path, size: Tuple[int, int], background_color: str
+    svg: Path, png: Path, size: Tuple[int, int], background_color: RgbHex
 ) -> None:
     """Convert an SVG file to a PNG file with the specified size."""
     try:
         subprocess.run(
             [
                 "inkscape",
-                svg_path,
+                svg,
                 "--export-type=png",
-                f"--export-filename={png_path}",
+                f"--export-filename={png}",
                 f"--export-width={size[0]}",
                 f"--export-height={size[1]}",
                 f"--export-background={background_color}",
@@ -67,66 +80,65 @@ def remove_image_metadata(image: Path) -> None:
         raise
 
 
-def new_image(
-    size: Tuple[int, int], color: str = "white", greyscale: bool = False
-) -> Image.Image:
+def new_image(size: Tuple[int, int], greyscale: bool = False) -> Image.Image:
     """Create a new blank image with the given width, height, and background color."""
     config = load_config(Path("config/config.toml"))
-    # print(f"config: {config}")  # ANCHOR - Debugging line
+    bg_color: tuple[int, int, int] = tuple(config["background_color"])
 
-    image = Image.new("RGB", size=size, color=color)
-    logo_info = config["logo"]
+    image = Image.new("RGB", size=size, color=bg_color)
+    logo_s: dict = config["logo"]
 
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(config["fonts"]["path"], size=20)
+    fonts = config["fonts"]
+    font = ImageFont.truetype(fonts["path"], size=20)
     header_fields = [("prompt", 10), ("name", 40), ("designation", 70)]
 
     for field, y in header_fields:
-        draw.text((50, y), config.get(field, ""), fill="black", font=font)
-    for logo in logo_info.keys():
-        username = logo_info[logo]["username"]
+        draw.text((50, y), config.get(field, ""), fill=fonts["color"], font=font)
+    for logo in logo_s.keys():
+        username = logo_s[logo]["username"]
         svg_to_png(
-            svg_path=Path(f"{logo_info[logo]['path']}".split(".")[0] + ".svg"),
-            png_path=Path(logo_info[logo]["path"]),
+            svg=Path(f"{logo_s[logo]['path']}".split(".")[0] + ".svg"),
+            png=Path(logo_s[logo]["path"]),
             size=LOGO_SIZE,
-            background_color=color,
+            background_color=rgb_to_hex(bg_color),
         )
-        with Image.open(f"{logo_info[logo]['path']}") as logo_img:
+        with Image.open(f"{logo_s[logo]['path']}") as _logo:
             image.paste(
-                logo_img,
+                _logo,
                 (
-                    logo_info[logo]["horizontal_offset"],
-                    logo_info[logo]["vertical_offset"],
+                    logo_s[logo]["horizontal_offset"],
+                    logo_s[logo]["vertical_offset"],
                 ),
             )
         draw.text(
             (
-                logo_info[logo]["horizontal_offset"] + LOGO_SIZE[0] + 10,
-                logo_info[logo]["vertical_offset"],
+                logo_s[logo]["horizontal_offset"] + LOGO_SIZE[0] + 10,
+                logo_s[logo]["vertical_offset"],
             ),
             f"{username}",
-            fill="black",
+            fill=fonts["color"],
             font=font,
         )
 
-    pictures = config["picture"]
-    for pic in pictures.keys():
-        pic_info = pictures[pic]
-        path = pic_info[0]
-        box: tuple = tuple(pic_info[1])
-        new_height = pic_info[2]
+    images: dict[str, list] = config["picture"]
+    for _image in images.keys():
+        image_info = images[_image]
+        path = image_info[0]
+        box: tuple = tuple(image_info[1])
+        new_height = image_info[2]
         try:
             remove_image_metadata(Path(path))
         except Exception as e:
             print(f"Skipping metadata removal for {path}: {e}")
-        with Image.open(path) as pic_img:
+        with Image.open(path) as picture:
             if greyscale:
-                pic_img = pic_img.convert("L")  # Convert to grayscale
-            aspect_ratio = pic_img.size[0] / pic_img.size[1]
+                picture = picture.convert("L")  # Convert to grayscale
+            aspect_ratio = picture.size[0] / picture.size[1]
             new_width = int(new_height * aspect_ratio)
-            print(f"Resizing {pic} to {new_width}x{new_height}")
-            pic_img = pic_img.resize((new_width, new_height))
-            image.paste(pic_img, box)
+            print(f"Resizing {_image} to {new_width}x{new_height}")
+            picture = picture.resize((new_width, new_height))
+            image.paste(picture, box)
             used_width = box[0] + new_width
             if used_width > (1400 - 10):  # 10px padding on right
                 raise ValueError(
